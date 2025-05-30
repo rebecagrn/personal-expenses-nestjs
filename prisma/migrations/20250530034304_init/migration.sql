5259e6e5f4ac06019a02e0a53a0171d75f8a299a
Rebeca Pacheco <rbkgarner@gmail.com>
2025-05-30 00:46:31 -0300

    chore: generate prisma migration

---
+++ prisma/migrations/20250530034304_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "expenses" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "expenses_pkey" PRIMARY KEY ("id")
+);
